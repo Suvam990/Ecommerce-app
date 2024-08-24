2e6b9ba229ac08b4fec177ef37a7ffc920d2c798
Suvam990 <subhamlama13@gmail.com>
2024-08-24 09:24:34 +0545

    Add files via upload

---
+++ phpEcommerce/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+    <button class="btn btn-primary">RENDING</button>
+
+    <script src="assets/js/jquery-3.6.0.min.js"></script>
+    <script src="assets/js/bootstrap.bundle.min.js"></script>
+
+
+
+</body>
+</html>
